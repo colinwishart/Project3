877de5bc9e377427897c9f4b0418e341389e52d1
ChrisReyno <119368993+ChrisReyno@users.noreply.github.com>
2023-12-04 21:09:27 -0500

    Update AdjList.h
    Changed map distance to double and moved the AdjacencyList to public

--- AdjList.h
+++ AdjList.h
@@ -7,6 +7,7 @@
 
 
 #include <map>
+#include <set>
 #include <unordered_map>
 #include <vector>
 
@@ -17,18 +18,13 @@ class AdjList {
 
     int numNodes; // total number of nodes for calculates
 
-private:
-
-    map<int, vector<pair<int, int>>> adjList;
-
 public:
-
+    map<int, vector<pair<int, double>>> adjList;
     AdjList(); // assigns member variables default values
-    void insertEdge(const int& from, const int& to, const int& weight); // adds a new link between two nodes
+    void insertEdge(const int& from, const int& to, const double& weight); // adds a new link between two nodes
     void printGraph();
     void numEdges();
-pair<double, vector<int>> findPaths(map<int, vector<pair<int, double>>> &graph, int start, double distance, int curr_node, double& dist_traversed, vector<int> &curr_path, vector<pair<double, vector<int>>> &paths, map<int, bool> &visited, int &step_count, double &bound_factor);
-
+    pair<double, vector<int>> findPaths(map<int, vector<pair<int, double>>> &graph, int start, double distance, int curr_node, double& dist_traversed, vector<int> &curr_path, vector<pair<double, vector<int>>> &paths, map<int, bool> &visited, int &step_count, double &bound_factor);
 };
 
 
