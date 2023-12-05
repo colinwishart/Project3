4a17c67c5a5a2c6d3cd426a11b61ee3fe59180c2
ChrisReyno <119368993+ChrisReyno@users.noreply.github.com>
2023-12-04 22:41:07 -0500

    Update AdjList.h
    .h from my comp

--- AdjList.h
+++ AdjList.h
@@ -18,18 +18,17 @@ using namespace std;
 /*************************CLASS DEFINITION*************************/
 class AdjList {
     int numNodes; // total number of nodes for calculates
-    map<int, vector<pair<int, double>>> adjList;
 
 public:
+    map<int, vector<pair<int, double>>> adjList;
     AdjList(); // assigns member variables default values
     void insertEdge(const int& from, const int& to, const double& weight); // adds a new link between two nodes
     void printGraph();
     void numEdges();
-    pair<double, vector<int>> findPaths(map<int, vector<pair<int, double>>> &graph, int start,
+    pair<double, vector<int>> dfsLoop(map<int, vector<pair<int, double>>> &graph, int start,
                                         double distance, int curr_node, double& dist_traversed,
                                         vector<int> &curr_path, vector<pair<double, vector<int>>> &paths,
                                         map<int, bool> &visited, int &step_count, double &bound_factor);
-    void PopulateFromFile(std::string filename);
 };
 
 
